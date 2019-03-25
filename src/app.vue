2b5bbdaa46ce753f77c5f59b81582397643fd023
tgd1106@163.com <tgd1106@163.com>
2019-03-25 18:57:27 +0800

    vue2.0项目搭建

---
+++ src/app.vue
@@ -0,0 +1,25 @@
+<template>
+    <div id="app" class="container">
+        {{ message }}
+        <p class="text">1111</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: 'tang2'
+        }
+    }
+}
+</script>
+
+<style scoped lang="scss">
+.container {
+    color: pink;
+    .text {
+        color: red;
+    }
+}
+</style>
---
+++ src/index.js
@@ -0,0 +1,18 @@
+// 入口文件
+import Vue from 'vue'
+import App from './app.vue'
+
+import './assets/styles/base.css'
+
+// new Vue({
+//     render: h => h(App)
+// }).$mount('#app')
+
+
+const root = document.createElement('div')
+document.body.appendChild(root)
+
+//mount就是讲我们的App挂载到root这样一个根节点中去
+new Vue({
+  render: (h) => h(App)
+}).$mount(root)
