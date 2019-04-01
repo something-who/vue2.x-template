84745005a26a99c57eac9d4ffef3083d2b52342d
tgd1106@163.com <tgd1106@163.com>
2019-04-01 18:54:28 +0800

    webpack

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -32,11 +32,11 @@ const config = {
         path: path.resolve(__dirname, '../dist')
     },
     plugins: [
-        new webpack.DefinePlugin({
-            'process.env': {
-                NODE_ENV: isDev ? '"development"' : '"production"'
-            }
-        }),
+        // new webpack.DefinePlugin({
+        //     'process.env': {
+        //         NODE_ENV: isDev ? '"development"' : '"production"'
+        //     }
+        // }),
         // 配置vue-loader插件
         new VueLoaderPlugin(),
         new HTMLPlugin({
@@ -58,22 +58,6 @@ const config = {
             test: /\.vue$/,
             loader: 'vue-loader'
         }, {
-            // 使用style-loader处理vue文件中的style标签里的css,或说是处理html中的style标签内的css
-            // css-loader处理从外部引入的css文件
-            test: /.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
-        }, {
-            // 对css预处理器scss的处理
-            test: /\.scss$/,
-            loader: [
-                'sass-loader',  // sass-loader依赖于node-loader
-                'style-loader',
-                'css-loader'
-            ]
-        }, {
             // 处理图片文件, 作用是将我们的图片转换成一个base64的字串存放于我们打包生成的js里面
             test: /\.(gif|jpg|jpeg|png|svg)$/,
             use: [{
@@ -85,7 +69,7 @@ const config = {
             }]
         }, {
             test: /\.js/,
-            // exclude 需要忽略的文件或文件夹下的文件
+            // exclude 需要忽略的文件或文件夹下的文件，反之，include
             exclude: /node_modules/,
             loader: 'babel-loader'
         }]
@@ -105,12 +89,53 @@ const config = {
     // }
 }
 
+// 开发环境和生产环境下的差异化配置
 if(isDev) {
+    // 开发环境下的配置
+
+    // rules-css相关
+    config.module.rules.push({
+        test: /\.css$/,
+        use: [
+            'style-loader',
+            {
+                loader: 'css-loader',
+                options: {
+                    importLoaders: 1
+                }
+            },
+            'postcss-loader'
+        ]
+    }, {
+        // 对scss的处理
+        test: /\.scss$/,
+        use: [
+            // 特别注意顺序
+            // 处理顺序是从sass-loader到style-loader
+            'style-loader',
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true
+                }
+            },
+            'sass-loader'
+        ]
+        // loader: [
+        //     'style-loader',
+        //     'css-loader',
+        //     'sass-loader'
+        // ]
+    })
+
+    // 开发环境服务器信息配置
     config.devServer = {
+        // contentBase: path.join(__dirname, 'dist'),
         port: 8000,
-        host: 'localhost',
+        host: '0.0.0.0', // 配置成0.0.0.0的话通过ip，localhost都能访问
         overlay: {
-            erros: true  // 编译的过程中能够让任何的错误都显示到网页上面
+            erros: true  // 编译的过程中，让任何的错误都显示到网页上面
         },
         open: false, // 自动打开浏览器
         hot: true // 热更新
@@ -120,6 +145,74 @@ if(isDev) {
         new webpack.HotModuleReplacementPlugin(), // 热更新插件
         new webpack.NoEmitOnErrorsPlugin()  // 不显示不必要的错误信息
     )
+
+    // 生成source map，方便调试
+    // https://webpack.docschina.org/configuration/devtool/#src/components/Sidebar/Sidebar.jsx
+    config.devtool = 'cheap-eval-source-map'
+} else {
+    // 生产环境下的配置
+
+    // 把css分离打包到单独的文件
+    config.module.rules.push({
+        test: /\.css$/,
+        use: [
+            // 使用的mini-css-extract-plugin插件实现将*.css从构建生成的js文件中分离出来
+            MiniCssExtractPlugin.loader,
+            {
+                loader: 'css-loader',
+                options: {
+                    importLoaders: 1
+                }
+            },
+            'postcss-loader'
+        ]
+    }, {
+        test: /\.scss$/,
+        use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            {
+                loader: 'postcss-loader',
+                options: {
+                    sourceMap: true
+                }
+            },
+            'sass-loader'
+        ]
+    })
+
+    config.plugins.push(
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "styles/style.[hash:8].css",
+            chunkFilename: "styles/style.[hash:8].css"
+        })
+    )
+
+    // 单独打包第三方类库文件（比如vue框架）
+    config.optimization = {
+        splitChunks: {
+            chunks: 'async',
+            minSize: 30000,
+            maxSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            automaticNameDelimiter: '~',
+            name: true,
+            cacheGroups: {
+                commons: {
+                    test: /[\\/]node_modules[\\/]/,
+                    name: 'vendors',
+                    chunks: 'all'
+                }
+            }
+        },
+        runtimeChunk: {
+            name: entrypoint => `runtimechunk~${entrypoint.name}`
+        }
+    }
 }
 
 module.exports = config
